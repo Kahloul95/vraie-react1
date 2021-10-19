5382121be459f49056bd55dc8cbfd63cb96c7517
kahloul <kahloulsami95@gmail.com>
2021-10-19 16:02:27 +0200

    jndnj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, FloatingLabel, Form, InputGroup, Row } from 'react-bootstrap';
 import './App.css';
+import Button from '@restart/ui/esm/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Container>
+     
+     <Row className="justify-content-md-center">
+       <Col xs lg="2">
+       <FloatingLabel controlId="floatingTextarea" label="Comments" className="mb-3">
+       <Form.Control as="textarea" placeholder="Leave a comment here" />
+     </FloatingLabel>
+       </Col>
+       <Col md="auto">
+       <Form>
+       <InputGroup hasValidation>
+     <InputGroup.Text>@</InputGroup.Text>
+     <Form.Control type="text" required isInvalid />
+     <Form.Control.Feedback type="invalid">
+       Please choose a username.
+     </Form.Control.Feedback>
+   </InputGroup>
+     <Form.Group className="mb-3" controlId="formBasicEmail">
+       <Form.Label>Email address</Form.Label>
+       <Form.Control type="email" placeholder="Enter email" />
+       <Form.Text className="text-muted">
+         We'll never share your email with anyone else.
+       </Form.Text>
+     </Form.Group>
+   
+     <Form.Group className="mb-3" controlId="formBasicPassword">
+       
+       <Form.Label>Password</Form.Label>
+       <Form.Control type="password" placeholder="Password" />
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="formBasicCheckbox">
+       <Form.Check type="checkbox" label="Check me out" />
+     </Form.Group>
+     <Button as="input" type="submit" value="Submit" ></ Button>{' '}
+   </Form>
+       </Col>
+       <Col xs lg="2">
+       <Form.Group controlId="formFileSm" className="mb-3">
+       <Form.Label>Importer Votre CV</Form.Label>
+       <Form.Control type="file" size="sm" />
+     </Form.Group>
+       </Col>
+     </Row>
+     
+   </Container>
     </div>
   );
 }
